2cd32e71587904e511b3a6a5fc87c883a930e47f
Michael Jake Buttigieg <157321812+MichaelJakeButtigieg@users.noreply.github.com>
2024-01-21 23:18:20 +0100

    Delete GeneFinder.R

--- GeneFinder.R
+++
@@ -1,4 +0,0 @@
-regionFilter <- read.csv(file = "/home/michaelj/Desktop/Coding/Thesis/CpGtoChrLoc/venv/lib/CpGmarkersandgeneID.csv", header = TRUE)
-cpgRegions <- read.csv(file = "/home/michaelj/Desktop/Coding/Thesis/CpGtoChrLoc/venv/lib/CpG_Sig_reg.csv", header = TRUE)
-
-
